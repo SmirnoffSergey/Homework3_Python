83b14201fef331bf91473edbf809d8ae57319be0
Sergey <s_smirnov@nisse.ru>
2022-10-06 17:24:03 +0300

    add homework 3

--- Ex5.py
+++ Ex5.py
@@ -4,16 +4,17 @@
 # - для k = 8 список будет выглядеть так:
 # [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]
 
-def fib(number):
-    if number in (1, 2):
-        return 1
-    return fib(number-1) + fib(number-2)
+number = int(input('Enter integer number: '))
+fib = [0, 1]
 
-n = int(input('Enter integer number: '))
-nega_fib = [0]
+for i in range(1, number):
+    fib.append(fib[i] + fib[i - 1])
 
-for i in range(1, n+1):
-    nega_fib.append(-fib(i))
-    nega_fib.append(fib(i))
-nega_fib.sort()
-print(nega_fib)
+nega_fib = fib[::-1]
+
+for i in range(0, number-1, 2):
+    nega_fib[i] = -nega_fib[i]
+
+nega_fib.pop(number)
+
+print(nega_fib + fib)
